b267b1eb4a191020846fb94ca9f55a41dc0e8020
Same-woo <133335869+same-woo@users.noreply.github.com>
2023-06-03 16:18:40 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = n/slice ;
+        
+        if (n%slice > 0) {
+            
+            answer += 1;
+        }
+        
+        
+        return answer;
+    }
+}
