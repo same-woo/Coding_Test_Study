be71ba74d2d032b172f3db332b767437975568d0
Same-woo <133335869+same-woo@users.noreply.github.com>
2023-06-10 14:34:04 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 0;
-        if (str1.contains(str2)) {
-            answer = 1;
-        } else {
-            answer = 2;
-        }
-
-        return answer;
+        return (str1.contains(str2)) ? 1:2;
     }
 }
