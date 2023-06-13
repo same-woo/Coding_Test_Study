ee8d35e3f77a043d9718f635359f9d36c588fab3
Same-woo <133335869+same-woo@users.noreply.github.com>
2023-06-13 20:39:35 +0900

    [unrated] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        
+         answer = my_string.repeat(k);
+        
+        return answer;
+    }
+}
