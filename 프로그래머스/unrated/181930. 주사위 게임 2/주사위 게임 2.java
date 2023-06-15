17c8a1a41a8b9dca6744a03f7e2cac1cccf7c911
Same-woo <133335869+same-woo@users.noreply.github.com>
2023-06-15 19:14:29 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        
+        if ( a != b && a != c && b != c) {
+            answer +=  a + b + c;
+        } else if (a == b && a == c && b == c) {
+            answer += (a+b+c)*(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2)) *(Math.pow(a,3)+Math.pow(b,3)+Math.pow(c,3));
+        }  else {
+            answer += (a+b+c)*(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2));
+        }
+        
+        return answer;
+    }
+}
