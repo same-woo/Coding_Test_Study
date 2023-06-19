f29a4702db191d946d831ffc18e66bea09faf205
Same-woo <133335869+same-woo@users.noreply.github.com>
2023-06-19 23:14:00 +0900

    [level 0] Title: 가까운 수, Time: 0.34 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -1,19 +1,16 @@
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
     public int solution(int[] array, int n) {
-        Arrays.sort(array); // 배열 정렬
+        int answer = 0;
+        int temp = 100;
         
-        int answer = array[0]; // 초기값은 첫 번째 원소로 설정
-        int minDiff = Math.abs(array[0] - n); // 초기값은 첫 번째 원소와의 차이로 설정
-        
-        for (int i = 1; i < array.length; i++) {
-            int diff = Math.abs(array[i] - n); // 현재 원소와 n의 차이 계산
+        Arrays.sort(array);
+        for (int i=0; i < array.length; i++) {
+            if (temp > Math.abs(array[i] - n)) {
+                temp = Math.abs(array[i] - n);
+                answer = array[i];
+            }   
             
-            if (diff < minDiff) { // 현재 원소와 n의 차이가 최소값보다 작으면
-                answer = array[i]; // 현재 원소를 정답으로 설정
-                minDiff = diff; // 최소값을 갱신
-            }
         }
         
         return answer;
