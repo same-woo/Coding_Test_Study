ee64ee988702eb5286a62ac99d7bf67de487b49e
Same-woo <133335869+same-woo@users.noreply.github.com>
2023-06-24 12:01:35 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.99 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        
+        for (int i = 0; i < n; i++) {
+            if (i % 2 != 0) {
+                answer += "박";
+            } else if (i % 2 == 0) {
+                answer += "수";
+            }
+        }
+        
+        return answer;
+    }
+}
