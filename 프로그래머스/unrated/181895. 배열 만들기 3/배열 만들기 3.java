9423e55c448e168ddf423feb423e37ef30231ee6
Same-woo <133335869+same-woo@users.noreply.github.com>
2023-06-25 21:41:43 +0900

    [unrated] Title: 배열 만들기 3, Time: 0.91 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,22 +1,22 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 
-public class Solution {
-    public static int[] solution(int[] arr, int[][] intervals) {
-        int[] firstInterval = Arrays.copyOfRange(arr, intervals[0][0], intervals[0][1] + 1);
-        int[] secondInterval = Arrays.copyOfRange(arr, intervals[1][0], intervals[1][1] + 1);
+class Solution {
 
-        int[] newArray = new int[firstInterval.length + secondInterval.length];
-        System.arraycopy(firstInterval, 0, newArray, 0, firstInterval.length);
-        System.arraycopy(secondInterval, 0, newArray, firstInterval.length, secondInterval.length);
+    public int[] solution(int[] arr, int[][] intervals) {
 
-        return newArray;
-    }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
+            for (int j = intervals[i][0]; j <= intervals[i][1]; j++) {
+                list.add(arr[j]);
+            }
+        }
 
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[][] intervals = {{2, 5}, {7, 9}};
+        int[] answer = new int[list.size()];
 
-        int[] result = solution(arr, intervals);
-        System.out.println(Arrays.toString(result));
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
     }
 }
