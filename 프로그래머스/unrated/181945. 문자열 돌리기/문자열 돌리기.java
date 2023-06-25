3637dd470fe2f97641e2e1b793096fdd5691915a
Same-woo <133335869+same-woo@users.noreply.github.com>
2023-06-25 21:44:26 +0900

    [unrated] Title: 문자열 돌리기, Time: 150.13 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        
+        String[] str = a.split("");
+    for (int i =0; i < str.length; i++){
+        System.out.println(str[i]);
+    }
+        
+    }
+}
