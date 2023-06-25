39a152eccb158e99bce2fee7715176ae4eb99b8f
Same-woo <133335869+same-woo@users.noreply.github.com>
2023-06-25 23:33:21 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int i = 1;
+        int factorial = 1;
+
+        while (factorial <= n) {
+            factorial *= i;
+            i++;
+        }
+
+        return i - 2;
+    }
+}
