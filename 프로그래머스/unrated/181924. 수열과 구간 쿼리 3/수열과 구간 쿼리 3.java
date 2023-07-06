1ae0081ddb38fe116129bac94f0909783144cbd6
Same-woo <133335869+same-woo@users.noreply.github.com>
2023-07-06 23:06:33 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.05 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int[] query : queries) {
+            int i = query[0];
+            int j = query[1];
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        return arr;
+    }
+}
