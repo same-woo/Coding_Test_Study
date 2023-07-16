a7088e4ef8c09aca0057470f4cb1800248a0d904
Same-woo <133335869+same-woo@users.noreply.github.com>
2023-07-16 20:22:50 +0900

    [unrated] Title: 리스트 자르기, Time: 0.04 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
@@ -1,42 +1,27 @@
+import java.util.*;
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
+        
         int a = slicer[0];
         int b = slicer[1];
         int c = slicer[2];
         
-        int[] answer;
+        if(n ==1)
+            return Arrays.copyOfRange(num_list, 0, b+1);
+        if(n==2)
+            return Arrays.copyOfRange(num_list, a, num_list.length);
+        if(n==3)
+            return Arrays.copyOfRange(num_list, a, b+1);
         
-        if (n == 1) {
-            answer = new int[b + 1];
-            for (int i = 0; i <= b; i++) {
-                answer[i] = num_list[i];
-            }
-        } else if (n == 2) {
-            answer = new int[num_list.length - a];
-            for (int i = a; i < num_list.length; i++) {
-                answer[i - a] = num_list[i];
-            }
-        } else if (n == 3) {
-            answer = new int[b - a + 1];
-            for (int i = a; i <= b; i++) {
-                answer[i - a] = num_list[i];
-            }
-        } else if (n == 4) {
-            int count = (b - a + 1) / c;
-            if ((b - a + 1) % c != 0) {
-                count++;
-            }
-            answer = new int[count];
-            
-            int index = 0;
-            for (int i = a; i <= b; i += c) {
-                answer[index++] = num_list[i];
-            }
-        } else {
-            // n이 1, 2, 3, 4 중 하나가 아닌 경우 예외 처리
-            answer = new int[0];
+      List<Integer> result = new ArrayList<>();
+        for (int i = a; i <= b; i += c) {
+            result.add(num_list[i]);
         }
         
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
         return answer;
     }
 }
